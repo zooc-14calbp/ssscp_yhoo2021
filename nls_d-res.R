2e213f13855613570b4d3589691804f83f6d5de6
zooc-14calbp <37280781+zooc-14calbp@users.noreply.github.com>
2021-02-07 16:05:54 +0900

    v 2.10
    R code fixed

--- nls_d-res.R
+++ nls_d-res.R
@@ -1,19 +1,22 @@
-getwd()
 #
-micro060 <- read.csv("d-res_60mm.csv", header = TRUE)
+##################################################################
+# Micro 60 mm
+#####
+rm(list = ls(all.names=TRUE))
+dat <- read.csv("d-res_60mm.csv", header = TRUE)
 #
-distance <- micro060$d
-resolution <- micro060$res
-pixel_width <- micro060$pw
+distance <- dat$d
+resolution <- dat$res
+pixel_width <- dat$pw
 #
-fit_res <- nls(resolution~a/distance+b,
-               start=c(a=1,b=1), trace = TRUE)
+fit_res <- nls(resolution~a/(distance+b)+c,
+               start=c(a=275850,b=2.189,c=213.9), trace = TRUE)
 summary(fit_res)
 #
 fit_pw <- nls(pixel_width~a*distance+b,
               start=c(a=1,b=1), trace = TRUE)
 summary(fit_pw)
-#
+plot.new()
 plot(
   distance, resolution,
   ylab = "",
@@ -23,7 +26,87 @@ plot(
   )
 lines(distance, fitted(fit_res), lty=1)
 axis(1)
-axies(2)
+axis(2)
+#
+par(new = TRUE)
+plot(
+  distance, pixel_width,
+  ylab = "",
+  xlab = "",
+  col="red",
+  axes = FALSE
+)
+lines(distance, fitted(fit_pw))
+axis(4)
+##################################################################
+# Micro 85 mm
+###
+rm(list = ls(all.names=TRUE))
+dat <- read.csv("d-res_85mm.csv", header = TRUE)
+#
+distance <- dat$d
+resolution <- dat$res
+pixel_width <- dat$pw
+#
+fit_res <- nls(resolution~a/(distance+b)+c,
+               start=c(a=381283.41,b=-70.47,c=222.24), trace = TRUE)
+summary(fit_res)
+#
+fit_pw <- nls(pixel_width~a*distance+b,
+              start=c(a=1,b=1), trace = TRUE)
+summary(fit_pw)
+#
+plot.new()
+plot(
+  distance, resolution,
+  ylab = "",
+  xlab = "",
+  col="blue",
+  axes = FALSE
+)
+lines(distance, fitted(fit_res), lty=1)
+axis(1)
+axis(2)
+#
+par(new = TRUE)
+plot(
+  distance, pixel_width,
+  ylab = "",
+  xlab = "",
+  col="red",
+  axes = FALSE
+)
+lines(distance, fitted(fit_pw))
+axis(4)
+##################################################################
+# Micro 105 mm
+###
+rm(list = ls(all.names=TRUE))
+dat <- read.csv("d-res_105mm.csv", header = TRUE)
+#
+distance <- dat$d
+resolution <- dat$res
+pixel_width <- dat$pw
+#
+fit_res <- nls(resolution~a/(distance+b)+c,
+               start=c(a=486838.037,b=-63.660,c=120.851), trace = TRUE)
+summary(fit_res)
+#
+fit_pw <- nls(pixel_width~a*distance+b,
+              start=c(a=1,b=1), trace = TRUE)
+summary(fit_pw)
+#
+plot.new()
+plot(
+  distance, resolution,
+  ylab = "",
+  xlab = "",
+  col="blue",
+  axes = FALSE
+)
+lines(distance, fitted(fit_res), lty=1)
+axis(1)
+axis(2)
 #
 par(new = TRUE)
 plot(
